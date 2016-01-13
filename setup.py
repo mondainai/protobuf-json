50e8e95483fbadb92529d3694d9f632947ae8d24
t.kanold <thomas.kanold@unister.de>
2016-01-13 15:39:36 +0100

    added anonymous setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='protobuf-json',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
